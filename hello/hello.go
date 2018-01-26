43b368db3d984e534174c40fd6e9025f7b98fcdd
tzx <semieye@qq.com>
2018-01-26 22:09:06 +0800

    add hello.go, types.go, reverse.go, reverse_test.go

---
+++ hello/hello.go
@@ -0,0 +1,14 @@
+//my first go program.
+package main
+
+import (
+	"fmt"
+
+	"github.com/semieye/learn-go/utils"
+)
+
+func main() {
+	fmt.Printf("Hello, world. \n")
+	fmt.Println(utils.Reverse("!selpmaxe oG ,olleH"))
+	fmt.Printf("Hello, world or 你好，世界 or καλημ ́ρα κóσμ or こんにちはせかい\n")
+}
---
+++ utils/reverse_test.go
@@ -0,0 +1,20 @@
+package utils
+
+import "testing"
+
+func TestReverse(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"Hello, 世界", "x界世 ,olleH"}, //A misstake
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
