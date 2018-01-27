e67a381e2517ddf73dea9710d3754fdd0d01e4c4
tzx <semieye@qq.com>
2018-01-27 21:24:40 +0800

    用Sequence实现sort.Interface的方法，实现排序后打印

--- basic/interface/interface.go
+++ basic/interface/interface.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 type phone interface {
@@ -31,4 +32,52 @@ func main() {
 	p = new(iPhone)
 	p.call()
 
+	var t interface{}
+	t = 0.4325345
+	switch t := t.(type) {
+	default:
+		fmt.Printf("unexpected type %T\n", t) // %T 输出 t 是什么类型
+	case bool:
+		fmt.Printf("boolean %t\n", t) // t 是 bool 类型
+	case int:
+		fmt.Printf("integer %d\n", t) // t 是 int 类型
+	case *bool:
+		fmt.Printf("pointer to boolean %t\n", *t) // t 是 *bool 类型
+	case *int:
+		fmt.Printf("pointer to integer %d\n", *t) // t 是 *int 类型
+	}
+
+	//用Sequence实现sort.Interface的方法，实现排序后打印
+	seq := Sequence{2, 1, 0, 3, 9, 7, 6, 5, 8, 4}
+	fmt.Println(seq.String())
+
+}
+
+// Sequence interface
+type Sequence []int
+
+// Methods required by sort.Interface.
+// sort.Interface 所需的方法。
+func (s Sequence) Len() int {
+	return len(s)
+}
+func (s Sequence) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s Sequence) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Method for printing - sorts the elements before printing.
+// 用于打印的方法 - 在打印前对元素进行排序。
+func (s Sequence) String() string {
+	sort.Sort(s)
+	str := "["
+	for i, elem := range s {
+		if i > 0 {
+			str += " "
+		}
+		str += fmt.Sprint(elem)
+	}
+	return str + "]"
 }
