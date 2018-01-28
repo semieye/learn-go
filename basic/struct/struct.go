1e021b1d5e8e39ad44969abd5b6ee92b19287e16
tzx <semieye@qq.com>
2018-01-28 11:56:26 +0800

    增加struct匿名字段演示

--- basic/struct/struct.go
+++ basic/struct/struct.go
@@ -10,6 +10,19 @@ type Books struct {
 	bookID  int
 }
 
+// Human a struct
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+// Student a struct include Human
+type Student struct {
+	Human      // 匿名字段，那么默认Student就包含了Human的所有字段
+	speciality string
+}
+
 func main() {
 	var Book1 Books /* 声明 Book1 为 Books 类型 */
 	var Book2 Books /* 声明 Book2 为 Books 类型 */
@@ -31,6 +44,8 @@ func main() {
 
 	/* 打印 Book2 信息 */
 	printBook(Book2)
+
+	printStudent()
 }
 
 func printBook(book Books) {
@@ -38,4 +53,27 @@ func printBook(book Books) {
 	fmt.Printf("Book author : %s\n", book.author)
 	fmt.Printf("Book subject : %s\n", book.subject)
 	fmt.Printf("Book bookID : %d\n", book.bookID)
+}
+
+func printStudent() {
+	// 我们初始化一个学生
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
+
+	// 我们访问相应的字段
+	fmt.Println("His name is ", mark.name)
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("His weight is ", mark.weight)
+	fmt.Println("His speciality is ", mark.speciality)
+	// 修改对应的备注信息
+	mark.speciality = "AI"
+	fmt.Println("Mark changed his speciality")
+	fmt.Println("His speciality is ", mark.speciality)
+	// 修改他的年龄信息
+	fmt.Println("Mark become old")
+	mark.age = 46
+	fmt.Println("His age is", mark.age)
+	// 修改他的体重信息
+	fmt.Println("Mark is not an athlet anymore")
+	mark.weight += 60
+	fmt.Println("His weight is", mark.weight)
 }
