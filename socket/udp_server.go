46787b6f1473ef8eda0f3488d57e443be692868c
tzx <semieye@qq.com>
2018-02-06 17:14:54 +0800

    socket tcp udp 基础学习

---
+++ socket/udp_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	service := ":1200"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+	conn, err := net.ListenUDP("udp", udpAddr)
+	checkError(err)
+	for {
+		handleClient(conn)
+	}
+}
+func handleClient(conn *net.UDPConn) {
+	var buf [512]byte
+	n, addr, err := conn.ReadFromUDP(buf[0:])
+	if err != nil {
+		return
+	}
+	fmt.Println("read size:", n, ", [", string(buf[:n]), "]")
+	daytime := time.Now().String()
+	conn.WriteToUDP([]byte(daytime), addr)
+}
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
